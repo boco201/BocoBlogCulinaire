6d37dd86abdac98361ae6ef1f493d4461ab110e5
boco <elizaboco66@gmail.com>
2020-09-12 17:44:52 +0200

    modif

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 //----------------------------------Route Admin Categories-------------------------------//
-Route::get('/admin', 'Admin\AdminCategoryController@index')->name('admin.categories.index');
+Route::get('/admin', 'Admin\AdminCategoryController@index');
 Route::get('/admin/categories/create', 'Admin\AdminCategoryController@create')->name('admin.categories.create');
 Route::post('/admin/categories', 'Admin\AdminCategoryController@store')->name('admin.categories.store');
 Route::get('/admin/categories/{category}/edit', 'Admin\AdminCategoryController@edit')->name('admin.categories.edit');
